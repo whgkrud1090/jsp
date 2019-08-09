06ee2625ed78ff28ff09562a24ce30637257680e
hkcho <gkrud1023@gamil.com>
2019-08-09 17:11:12 +0900

    servier side cookie  - 웹브라우저가 요청시 보낸 쿠키의 정보 저장  - request.getCookies();
     - 서버가 응답 생성시 클라이언트에게
       새로운 쿠키를 생성하도록 지시
     - response.addCookie(Cookie);

--- src/main/java/kr/or/ddit/login/web/LoginController.java
+++ src/main/java/kr/or/ddit/login/web/LoginController.java
@@ -3,6 +3,7 @@ package kr.or.ddit.login.web;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,6 +33,20 @@ public class LoginController extends HttpServlet {
 	*/
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//webapp/jsp/login.jsp --> jsp/login.jsp
+		
+		//웹브라우저가 보낸 cookie 확인
+		Cookie[] cookies = request.getCookies();
+		for(Cookie cookie : cookies) {
+			logger.debug("cookie name : {}, cookis value : {}", cookie.getName(), cookie.getValue());
+			
+		}
+		//응답을 생성할 때 웹브라우저에세 쿠키를 저장할 것을 지시
+		
+		Cookie cookie = new Cookie("serverGen", "serverValue");
+		//7일의 유효기간을 갖는 쿠키
+		cookie.setMaxAge(60*60*24*7);
+		response.addCookie(cookie);
+		
 		request.getRequestDispatcher("/login/login.jsp").forward(request, response);
 	}
 
@@ -62,18 +77,24 @@ public class LoginController extends HttpServlet {
 		IUserDao userDao = new UserDao();
 		User user = userDao.getUser(userId);
 		
-		if(user.checkLoginValidate(userId, pass)) {
+		//db에 존재하지 않는 사용자 체크
+		if(user == null) { //-> 로그인 화면으로 이동
+			doGet(request, response);
+			
+		}else if(user.checkLoginValidate(userId, pass)) {
 		//사용자가 입력한 파라미터 정보와 db에서 조회해온 값이 동일할 경우
 		//		--> webapp/mein.jsp
 		
 		//사용자가 입력한 파라미터 정보와 db에서 조회해온 값이 동일하지 않을 경우
 		//		-->webapp/login/login.jsp
 			
+			 	
 			HttpSession session = request.getSession();
 			logger.debug("session.getId() : {}", session.getId());
 			session.setAttribute("S_USERVO", user);
 			
 			request.getRequestDispatcher("/main.jsp").forward(request, response);
+			
 		}else {
 			//forward의 경우 request, response객체를 공유
 			//request method도 같이 공유
